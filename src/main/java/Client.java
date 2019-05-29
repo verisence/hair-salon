e642618b278d257a003341013b26a9baf820bff9
Kipkemboi Phil <Kipkemboiphil@gmail.com>
2019-05-29 09:55:35 +0300

    Add a database rule to the ClientTest class to handle connection and clearing the database.

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,2 +1,8 @@
 public class Client {
+    private int id;
+    private String name;
+    private String description;
+    private String Email;
+    private String phone;
+
 }
--- src/test/java/ClientTest.java
+++ src/test/java/ClientTest.java
@@ -1,2 +1,8 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+import java.util.Arrays;
+
 public class ClientTest {
+    @Rule
+    public DatabaseRule databaseRule = new DatabaseRule();
 }
