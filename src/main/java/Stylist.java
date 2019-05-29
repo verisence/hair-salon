298833a507183da4b22ade042dc53dc2db4ad1ea
Kipkemboi Phil <Kipkemboiphil@gmail.com>
2019-05-29 12:28:02 +0300

    Add DatabaseRule to StylistTest and test that stylist instantiates correctly.

--- src/main/java/Stylist.java
+++ src/main/java/Stylist.java
@@ -1,2 +1,15 @@
+import java.util.List;
+import org.sql2o.*;
+
 public class Stylist {
+    private int id;
+    private String name;
+    private String email;
+    private String phone;
+
+    public Stylist(String name, String email, String phone){
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+    }
 }
--- src/test/java/StylistTest.java
+++ src/test/java/StylistTest.java
@@ -1,2 +1,14 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
 public class StylistTest {
+    @Rule
+    public DatabaseRule databaseRule = new DatabaseRule();
+    
+    @Test
+    public void stylist_instantiatesCorrectly_true() {
+        Stylist testStylist = new Stylist("Terry", "terry@mail.mail", "998877");
+        assertTrue(testStylist instanceof Stylist);
+    }
+    
 }
