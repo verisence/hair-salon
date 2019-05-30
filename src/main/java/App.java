a1f0e7cbf0f11df7ea9d2a438ee8d1157256bbb9
Kipkemboi Phil <Kipkemboiphil@gmail.com>
2019-05-30 08:25:22 +0300

    Add a / route

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -9,6 +9,10 @@ public class App {
         staticFileLocation("/public");
         String layout = "templates/layout.vtl";
 
-
+        get("/",(request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            model.put("template", "templates/index.vtl");
+            return new ModelAndView(model,layout);
+        },new VelocityTemplateEngine());
     }
 }
