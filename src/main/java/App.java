89eb41aea6f69492a49824dc3a2b7fbca6511d25
Kipkemboi Phil <Kipkemboiphil@gmail.com>
2019-05-30 12:05:26 +0300

    Add a route to a specific stylist that lists all the clients under a stylist and add/edit template files for the stylist

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -27,5 +27,25 @@ public class App {
             model.put("template", "templates/stylist-form.vtl");
             return new ModelAndView(model, layout);
         }, new VelocityTemplateEngine());
+
+        get("/stylists/:id", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            Stylist stylist = Stylist.find(Integer.parseInt(request.params(":id")));
+            model.put("stylist", stylist);
+            model.put("template", "templates/stylist.vtl");
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
+        post("/stylists", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            String name = request.queryParams("name");
+            String email = request.queryParams("email");
+            String phone = request.queryParams("phone");
+            Stylist stylist = new Stylist(name, email, phone);
+            stylist.save();
+            model.put("template", "templates/stylists.vtl");
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
+
+
     }
 }
