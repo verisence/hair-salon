4a180093f1e1a74e105371d7e3448c108898604e
Kipkemboi Phil <Kipkemboiphil@gmail.com>
2019-05-31 14:58:56 +0300

    Add an outfile for the database

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,7 +5,16 @@ import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
 
 public class App {
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
+
     public static void main(String[] args){
+        port(getHerokuAssignedPort());
         staticFileLocation("/public");
         String layout = "templates/layout.vtl";
 
--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -2,4 +2,8 @@ import org.sql2o.*;
 
 public class DB {
     public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/hair_salon", "verisence", "pass");
+//public static Sql2o sql2o = new Sql2o("jdbc:postgres://nshbdsznahsewo" ":8257d9c735336da936578284e1a354fd5e4538f79b5d81aa1292c63bcc7b744f@ec2-54-225-106-93.compute-1.amazonaws"  ".com:5432/dee8kmhg0m4ol1", "nshbdsznahsewo",  "8257d9c735336da936578284e1a354fd5e4538f79b5d81aa1292c63bcc7b744f");//!
+
+
+
 }
